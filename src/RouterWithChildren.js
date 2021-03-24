634f74e6c4654f4ab650b812e6b6439336dcbdd6
kam <331677620@qq.com>
2021-03-24 14:09:07 +0800

    add: Switch to RouterWithChildren

--- src/RouterWithChildren.js
+++ src/RouterWithChildren.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Route, Router } from 'react-router';
+import { Route, Router, Switch } from 'react-router';
 
 const RevRoutes = ({ routes = [] }) => {
-  return routes.map(({ children, path, component = null, exact, indexRoute, ...props }) => {
-    const Page = component;
-    const childrenComp = <RevRoutes routes={children} />;
-    return (
-      <Route
-        key={path}
-        path={path}
-        exact={exact}
-        render={(childProps) => {
-          if (component) {
-            return <Page {...childProps}>{childrenComp}</Page>;
-          } else {
-            return childrenComp;
-          }
-        }}
-        {...props}
-      />
-    );
-  });
+  return (
+    <Switch>
+      {routes.map(({ children, path, component = null, exact, indexRoute, ...props }) => {
+        const Page = component;
+        const childrenComp = <RevRoutes routes={children} />;
+        return (
+          <Route
+            key={path}
+            path={path}
+            exact={exact}
+            render={(childProps) => {
+              if (component) {
+                return <Page {...childProps}>{childrenComp}</Page>;
+              } else {
+                return childrenComp;
+              }
+            }}
+            {...props}
+          />
+        );
+      })}
+    </Switch>
+  );
 };
 
 const RouterWithChildren = ({ routes = [], ...props }) => {
